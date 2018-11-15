5b8ae0697cf0d39346edf815fa4d2a96bdbacedc
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2018-11-15 09:20:46 -0800

    Updated gitignore

--- packages/zefyr/example/lib/main.dart
+++ packages/zefyr/example/lib/main.dart
@@ -2,9 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 import 'package:flutter/material.dart';
-
-import 'src/full_page.dart';
 import 'src/form.dart';
+import 'src/full_page.dart';
 
 void main() {
   runApp(new ZefyrApp());
