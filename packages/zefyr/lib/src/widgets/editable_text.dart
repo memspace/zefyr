614fa2b34b5e7324851f97108fe7cd9d9fb9757f
탁동혁 <tdh8316@naver.com>
2019-05-13 23:07:36 +0900

    Replace cupertinoTextSelectionControls with materialTextSelectionControls (#57)  - See https://github.com/flutter/flutter/issues/22624

--- packages/zefyr/lib/src/widgets/editable_text.dart
+++ packages/zefyr/lib/src/widgets/editable_text.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, the Zefyr project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:notus/notus.dart';
 
@@ -117,7 +117,7 @@ class _ZefyrEditableTextState extends State<ZefyrEditableText>
     if (widget.enabled) {
       layers.add(ZefyrSelectionOverlay(
         controller: widget.controller,
-        controls: cupertinoTextSelectionControls,
+        controls: materialTextSelectionControls,
         overlay: overlay,
       ));
     }
