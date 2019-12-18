4a8b5fdad83c2efc09877ffd124e8386d2e8b810
masewo <masewo@gmx.net>
2019-12-18 14:04:29 +0100

    implementing showAutocorrectionPromptRect in InputConnectionController

--- packages/zefyr/lib/src/widgets/input.dart
+++ packages/zefyr/lib/src/widgets/input.dart
@@ -166,6 +166,11 @@ class InputConnectionController implements TextInputClient {
     }
   }
 
+  @override
+  void showAutocorrectionPromptRect(int start, int end) {
+    // TODO: implement showAutocorrectionPromptRect
+  }
+
   //
   // Private members
   //
