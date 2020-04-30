6fbd34aca28e18e35ef1b2b08d87e27669c969b0
julian <julian.daroqui@happydonia.com>
2020-04-30 20:19:00 +0200

    LInk in view mode

--- packages/zefyr/lib/src/widgets/common.dart
+++ packages/zefyr/lib/src/widgets/common.dart
@@ -123,25 +123,7 @@ class _ZefyrLineState extends State<ZefyrLine> {
     final List<TextSpan> children = widget.node.children
         .map((node) => _segmentToTextSpan(node, theme, scope))
         .toList(growable: false);
-
-        GestureRecognizer recognizer;
-
-        if (attrs.contains(NotusAttribute.link)) {
-          final tapGestureRecognizer = TapGestureRecognizer();
-          tapGestureRecognizer.onTap = () {
-            print("delegate: ${scope.attrDelegate}");
-            if (scope.attrDelegate?.onLinkTap != null) {
-              scope.attrDelegate.onLinkTap(attrs.get(NotusAttribute.link).value);
-            }
-          };
-          recognizer = tapGestureRecognizer;
-        }
-
-    return TextSpan(
-      text: segment.value,
-      recognizer: recognizer,
-      style: _getTextStyle(attrs, theme),
-    );
+    return TextSpan(style: widget.style, children: children);
   }
 
   TextSpan _segmentToTextSpan(
