579e6ef26f9f985bf824d3526469fa7a708a5662
masewo <masewo@gmx.net>
2020-05-09 11:05:36 +0200

    added clipboardstatusnotifier

--- packages/zefyr/lib/src/widgets/selection.dart
+++ packages/zefyr/lib/src/widgets/selection.dart
@@ -597,7 +597,8 @@ class _SelectionToolbarState extends State<_SelectionToolbar> {
         block.preferredLineHeight,
         midpoint,
         endpoints,
-        widget.selectionOverlay);
+        widget.selectionOverlay,
+        ClipboardStatusNotifier());
     return CompositedTransformFollower(
       link: block.layerLink,
       showWhenUnlinked: false,
