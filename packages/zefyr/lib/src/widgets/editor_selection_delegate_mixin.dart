dad98a2ae9692c7311cbe4f035dafe47503e661a
Long <harryle.fit@gmail.com>
2021-02-04 14:42:54 +0700

    add codes to parse data

--- packages/zefyr/lib/src/widgets/editor_selection_delegate_mixin.dart
+++ packages/zefyr/lib/src/widgets/editor_selection_delegate_mixin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:notus/notus.dart';
 
 import 'editor.dart';
@@ -12,8 +13,41 @@ mixin RawEditorStateSelectionDelegateMixin on EditorState
 
   @override
   set textEditingValue(TextEditingValue value) {
-    widget.controller
-        .updateSelection(value.selection, source: ChangeSource.local);
+    if (value.text == textEditingValue.text) {
+      widget.controller.updateSelection(value.selection);
+    } else {
+      __setEditingValue(value);
+    }
+  }
+
+  void __setEditingValue(TextEditingValue value) async {
+    if (await __isItCut(value)) {
+      widget.controller.replaceText(
+        textEditingValue.selection.start,
+        textEditingValue.text.length - value.text.length,
+        '',
+        selection: value.selection,
+      );
+    } else {
+      final value = textEditingValue;
+      final data = await Clipboard.getData(Clipboard.kTextPlain);
+      if (data != null) {
+        final length =
+            textEditingValue.selection.end - textEditingValue.selection.start;
+        widget.controller.replaceText(
+          value.selection.start,
+          length,
+          data.text,
+          selection: value.selection,
+        );
+      }
+    }
+  }
+
+  Future<bool> __isItCut(TextEditingValue value) async {
+    final data = await Clipboard.getData(Clipboard.kTextPlain);
+
+    return textEditingValue.text.length - value.text.length == data.text.length;
   }
 
   @override
