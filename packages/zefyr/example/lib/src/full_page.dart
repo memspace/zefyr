1e4514d113433f3a8633934abfbd65986c1dcd4d
Alan Mantoux <alan.mantoux@gmail.com>
2021-02-26 18:33:32 +0100

    Fix example

--- packages/zefyr/example/lib/src/full_page.dart
+++ packages/zefyr/example/lib/src/full_page.dart
@@ -65,7 +65,7 @@ class _FullPageEditorScreenState extends State<FullPageEditorScreen> {
         ? IconButton(onPressed: _stopEditing, icon: Icon(Icons.save))
         : IconButton(onPressed: _startEditing, icon: Icon(Icons.edit));
     final result = Scaffold(
-      resizeToAvoidBottomPadding: true,
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: ZefyrLogo(),
         actions: [
