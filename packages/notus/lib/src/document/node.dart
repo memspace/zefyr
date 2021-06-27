59eca912897d34a5eea46fb790845ce1f92cde70
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2021-06-26 20:24:46 -0700

    Removed import of meta package

--- packages/notus/lib/src/document/node.dart
+++ packages/notus/lib/src/document/node.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 import 'dart:collection';
 
-import 'package:meta/meta.dart';
 import 'package:quill_delta/quill_delta.dart';
 
 import 'attributes.dart';
