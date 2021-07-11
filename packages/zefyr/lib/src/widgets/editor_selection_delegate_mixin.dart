001e9600481884df05e902bfad16582f8eb2c4e1
son <son@TakerunoMacBook-Pro.local>
2021-07-11 14:57:04 +0900

    add commnet

--- packages/zefyr/lib/src/widgets/editor_selection_delegate_mixin.dart
+++ packages/zefyr/lib/src/widgets/editor_selection_delegate_mixin.dart
@@ -57,6 +57,7 @@ mixin RawEditorStateSelectionDelegateMixin on EditorState
 
               if (isURL(data.text)) {
                 // URLの場合はURLと認識させるために最後尾にスペースを追加する
+                // NOTE: 一度に `data.text + ' '`のようにreplaceTextしても意味ないので２度更新かけてる
                 widget.controller.replaceText(
                   widget.controller.selection.baseOffset,
                   0,
