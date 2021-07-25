54b00b4329108ba16ef7d8279362c2d4713977e5
YamamuraYuya <sales@best-rise.monster>
2021-07-25 18:10:23 +0900

    囲み枠の背景色をアルファ50から不透明度50％に変更した

--- packages/zefyr/lib/src/widgets/theme.dart
+++ packages/zefyr/lib/src/widgets/theme.dart
@@ -230,7 +230,7 @@ class ZefyrThemeData {
         ),
         spacing: VerticalSpacing(top: 16, bottom: 0),
         decoration: BoxDecoration(
-          color: Color(0xffF1F1F1).withAlpha(50),
+          color: Color(0xffF1F1F1).withOpacity(0.5),
           border: Border.all(
             color: Color(0xffF1F1F1),
             width: 1,
