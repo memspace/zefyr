bd1b23eebd8be599b5d7f7265d8579374c55b930
Alan Mantoux <alan.mantoux@gmail.com>
2021-08-07 21:23:09 +0200

    Fix copy/paste (#525)

--- packages/zefyr/lib/src/widgets/editor_selection_delegate_mixin.dart
+++ packages/zefyr/lib/src/widgets/editor_selection_delegate_mixin.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:notus/notus.dart';
+import 'package:zefyr/util.dart';
 
 import 'editor.dart';
 
@@ -12,8 +12,19 @@ mixin RawEditorStateSelectionDelegateMixin on EditorState
 
   @override
   set textEditingValue(TextEditingValue value) {
-    widget.controller
-        .updateSelection(value.selection, source: ChangeSource.local);
+    final cursorPosition = value.selection.extentOffset;
+    final oldText = widget.controller.document.toPlainText();
+    final newText = value.text;
+    final diff = fastDiff(oldText, newText, cursorPosition);
+    widget.controller.replaceText(
+        diff.start, diff.deleted.length, diff.inserted,
+        selection: value.selection);
+  }
+
+  @override
+  void userUpdateTextEditingValue(
+      TextEditingValue value, SelectionChangedCause cause) {
+    textEditingValue = value;
   }
 
   @override
